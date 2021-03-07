915bb3762822bee73e18fadd9174852065686fbf
Muhammad Hassan Raza <sshthassanraza@hotmail.com>
2021-03-07 18:58:15 +0100

    First Button Added

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInPage extends StatelessWidget {
@@ -6,7 +7,35 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
-        backgroundColor: Colors.black,
+        elevation: 2.0,
+      ),
+      body: _buildContainer(),
+    );
+  }
+
+  Widget _buildContainer() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          RaisedButton(
+            child: Text('Sign in with Google'),
+            onPressed: () {
+              print('Button Pressed');
+            },
+          ),
+        ],
       ),
     );
   }
