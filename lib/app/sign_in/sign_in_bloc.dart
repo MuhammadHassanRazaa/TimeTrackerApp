d249f1dea4a33787730505c22c15896b97cf0163
Muhammad Hassan Raza <sshthassanraza@hotmail.com>
2021-04-08 00:09:41 +0200

    Replaced Value Notifier with Stream Builder in SignIn Page

--- lib/app/sign_in/sign_in_bloc.dart
+++ lib/app/sign_in/sign_in_bloc.dart
@@ -4,24 +4,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_app/services/auth_base.dart';
 
 class SignInBloc {
-  SignInBloc({@required this.auth});
+  SignInBloc({@required this.isLoading, @required this.auth});
   final AuthBase auth;
+  final ValueNotifier<bool> isLoading;
 
-  final StreamController<bool> _isLoadingController = StreamController<bool>();
-  Stream<bool> get isLoadingStream => _isLoadingController.stream;
 
-  void dispose() {
-    _isLoadingController.close();
-  }
-
-  void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
 
   Future<User> _signIn(Future<User> Function() signInMethod) async {
     try {
-      _setIsLoading(true);
+      isLoading.value = true;
       return await signInMethod();
     } catch (e) {
-      _setIsLoading(false);
+     isLoading.value = false;
       rethrow;
     }
   }
